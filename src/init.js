7d4e5b0286a0ffc8286af5b578eb584254877466
luigicallero <luigicallero@gmail.com>
2021-08-30 19:39:38 -0300

    Final review on FrontEnd by Juan

--- src/init.js
+++ src/init.js
@@ -9,12 +9,12 @@ function Init(){
         <div className='players'>  
                       
                             
-                    <Link to="#" className='btn-link' id="player1Link">
+                    <Link to="/Player1" className='btn-link' id="player1Link">
                     <div className="play-div1">
                         <h4 className='play1'></h4>
                     </div>
                     </Link>
-                    <Link to="/#" className='btn-link' id="player2Link">
+                    <Link to="/Player2" className='btn-link' id="player2Link">
                     <div className="play-div2">
                         <h4 className='play2'></h4>
                     </div>    
