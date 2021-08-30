3734099d25f6d6e82590850f0eccb71a2186c209
luigicallero <luigicallero@gmail.com>
2021-08-30 14:55:08 -0300

    Checking nft owner vs wallet address

--- src/index.js
+++ src/index.js
@@ -9,13 +9,18 @@ import twitter from './img/twitter.svg'
 import instagram from './img/instagram.svg'
 import NFTMANJI from "./build/contracts/NFTMANJI.json"
 const Web3 = require('web3');
-var Contract = require('web3-eth-contract');
-
+let Contract = require('web3-eth-contract');
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 window.onload = () => {
   // Variables
   //let web3;
   let walletAddress;
+  let tokenOwner0;
+  let tokenOwner1;
+  let tokenImage0;
+  let tokenImage1;
+  let readObj;
 
   // Elements
   const connectButton = document.getElementById('wallet');
@@ -28,36 +33,49 @@ window.onload = () => {
   // Initial State of things
   player1Link.innerText = "Button Disabled";
   player2Link.innerText = "Button Disabled";
-    // if already connected to Web3 provider bring account data:
-
-  // Capture Contract Information from NFT contract NFTMANJI: xxxx
-  const web3 = new Web3('https://rinkeby.infura.io/v3/30a836a6de45424f8e6a6d514aeea2f3')
-  //web3.eth.getBlock('latest').then(result => console.log(result))
   
-  // set provider for all later instances to use
-  //Contract.setProvider('https://rinkeby.infura.io/v3/30a836a6de45424f8e6a6d514aeea2f3');
-  //var contract = new Contract(NFTMANJI.abi, '0x5239cE62Ceed3c5a69BACCa7a824A8e127D907aC')
-  //console.log(contract)
+  // if already connected to Web3 provider bring account data:
 
-  //const web3 = new Web3("https://rinkeby.infura.io/v3/30a836a6de45424f8e6a6d514aeea2f3")
+  // Capture Contract Information from NFT contract NFTMANJI: 0x5239cE62Ceed3c5a69BACCa7a824A8e127D907aC
+  const web3 = new Web3('https://rinkeby.infura.io/v3/30a836a6de45424f8e6a6d514aeea2f3')
   const NFTMANJIContract = new web3.eth.Contract ( NFTMANJI.abi, '0x5239cE62Ceed3c5a69BACCa7a824A8e127D907aC')
 
+  // Getting NFT images from NFT contract
   NFTMANJIContract.methods.getTokenURI(0).call(function (err, res) {
     if (err) {
       console.log("An error occured", err)
       return
     }
-    console.log("Token URI is: ", res)
+    tokenImage0 = res
+    console.log(tokenImage0)
   })
   
+  async function getImages() {
+    const response = await fetch(tokenImage0);
+    if(!response.ok)
+      throw new Error(response.statusText);
+    const json = await response.json();
+    console.log(json.image)
+  }
+  getImages()
+
+  // Getting NFT owners
   NFTMANJIContract.methods.ownerOf(0).call(function (err, res) {
     if (err) {
       console.log("An error occured", err)
       return
     }
-    console.log("Token Owner: ", res)
+    tokenOwner0 = res
   })
-  
+
+  NFTMANJIContract.methods.ownerOf(1).call(function (err, res) {
+    if (err) {
+      console.log("An error occured", err)
+      return
+    }
+    tokenOwner1 = res
+  })
+
   //contract.methods.somFunc().send({from: ....})
   //.on('receipt', function(){
   //    ...
@@ -67,50 +85,73 @@ window.onload = () => {
   // Functions
   const connect = async () => {
     if (window.ethereum) {
-      //web3 = new Web3(window.ethereum);
 
       window.ethereum.request({ method: 'eth_requestAccounts' })
         .then((accounts) => {        
-        walletAddress = accounts[0];
-        console.log(accounts);
+        walletAddress = web3.utils.toChecksumAddress(accounts[0]); // to bring the wallet address back to checksum format (mixed-case)
         account.innerText = walletAddress;
-        player1Link.innerText = "Enable button Player1"; //  habilitar el botón para el player 1
-        player2Link.innerText = "Enable button Player2"; //  habilitar el botón para el player 2
+        if ( tokenOwner0 == walletAddress ) {
+          player1Link.innerText = "Enabled button Player1"; //  habilitar el botón para el player 1    
+        }
+        if ( tokenOwner1 == walletAddress ) {
+          player2Link.innerText = "Enabled button Player2"; //  habilitar el botón para el player 2    
+        }
+
+
+        // Sending NFT
+        // another way:
+        /* async function sendNFT() {
+          const nonce = await web3.eth.getTransactionCount(walletAddress, 'latest'); //get latest nonce
+        
+          //the transaction
+          const tx = {
+            'from': walletAddress,
+            'to': '0xB02D2Abd985a5fa31B45D87a9297Bd22f8BaD931',
+            'nonce': nonce,
+            'gas': 500000,
+            'maxPriorityFeePerGas': 1999999987,
+            'data': NFTMANJIContract.methods.safeTransferFrom(walletAddress, '0xB02D2Abd985a5fa31B45D87a9297Bd22f8BaD931', 0).encodeABI()
+          };
+        
+          const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+          signPromise.then((signedTx) => {
+
+            web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(err, hash) {
+              if (!err) {
+                console.log("The hash of your transaction is: ", hash, "\nCheck Alchemy's Mempool to view the status of your transaction!"); 
+              } else {
+                console.log("Something went wrong when submitting your transaction:", err)
+              }
+            });
+          }).catch((err) => {
+            console.log(" Promise failed:", err);
+          });
+        }
+        sendNFT()
+
+        // this one is not failing but not transfering
+        NFTMANJIContract.methods.safeTransferFrom( walletAddress, '0xB02D2Abd985a5fa31B45D87a9297Bd22f8BaD931', 0 ).call().then(function(receipt){
+          console.log(receipt)
+        });
+        */
+
       }
       );
 
-
-/*
-      try {
-        
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-        const accounts = await web3.eth.getAccounts();
-        walletAddress = accounts[0];
-        console.log(walletAddress);
-        account.innerText = walletAddress;
-
-        // Capturing data from the contract
-        /* const nftOcaOwner = NFTMANJIContract[0];
-
-        
-
-        if ( walletAddress == nftOcaOwner ) {
-          player1Link.innerText = "Enable button Player1"; //  habilitar el botón para el player 1
-        }
-        
-        */
-
-//        content.style.display = 'initial'; // Juancho revisá por que no lo desactiva al botón, debe ser otro estilo
-//        connectButton.style.display = 'none'; // seguramente por eso salta al catch (err)
-        
-//      } catch (err) {
-//        alert('Please accept the request');
-//      }
     } else {
       alert('Web3 is required. You should consider trying MetaMask!');
     }
   };
+
+
+/*  
+  myContract.methods.myMethod(123).send({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'})
+.then(function(receipt){
+    // receipt can also be a new contract instance, when coming from a "contract.deploy({...}).send()"
+});
+*/
+  
+
 
   // Listeners
   connectButton.onclick = connect;
